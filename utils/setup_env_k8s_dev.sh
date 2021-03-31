e800998e495f51735d0dc9057e6fb764b9dbf0d4
Jonathan Henrique Medeiros <jonathan.medeiros@objective.com.br>
2021-03-31 20:58:52 -0300

    Fix docker driver to minikube installation

--- utils/setup_env_k8s_dev.sh
+++ utils/setup_env_k8s_dev.sh
@@ -128,7 +128,7 @@ install_minikube() {
     sudo curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && \
     sudo install minikube /usr/local/bin/minikube && \
     sudo rm minikube && \
-    minikube start && \
+    minikube start --driver=docker && \
     minikube addons enable ingress && \
     minikube addons enable dashboard && \
     minikube addons enable metrics-server && \
